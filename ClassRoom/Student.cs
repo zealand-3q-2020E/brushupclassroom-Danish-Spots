445ee3544f9accbf4dc35df4f2165662f2618ca9
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-08-26 12:40:06 +0200

    Created student class, added properties name, birth month and birthday. Made properties private set Constructor requires 3 parameters to create an object

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        public Student(string name, int birthMonth, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            Birthday = birthday;
+        }
+
+        public string Name { get; private set }
+        public int BirthMonth { get; private set }
+        public int Birthday { get; private set }
+    }
+}
