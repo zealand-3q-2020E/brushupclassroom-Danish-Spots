6d24c0866c4de66263628da748bbefc433608fcf
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-08-26 12:53:34 +0200

    Added console.writeline statements to print information

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -12,6 +12,15 @@ namespace ClassRoom
         {
             ClassRoom programming = new ClassRoom() { ClassName = "3Q Beginning of the semester", SemesterStart = new DateTime(2020, 8, 24), 
                 ClassList = new List<Student>() { new Student("Justin", 12, 31), new Student("Andreas", 2, 7), new Student("Dominik", 11, 9) } };
+
+            Console.WriteLine(programming.ClassName);
+            Console.WriteLine(programming.SemesterStart);
+            foreach (Student student in programming.ClassList)
+            {
+                Console.WriteLine(student.Name);
+            }
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
         }
     }
 }
