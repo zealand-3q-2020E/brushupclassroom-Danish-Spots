65d9b777a3f8d8f0937134bc3485052ced0c2299
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-08-26 13:02:07 +0200

    Added a method that checks the birthmonth of a student and returns a corresponding season

--- ClassRoom/Student.cs
+++ ClassRoom/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,5 +19,39 @@ namespace ClassRoom
         public string Name { get; private set; }
         public int BirthMonth { get; private set; }
         public int Birthday { get; private set; }
+
+        public string GetSeasonStudentBirthDay()
+        {
+
+            switch (BirthMonth)
+            {
+                case 12:
+                    return "Winter";
+                case 1:
+                    return "Winter";
+                case 2:
+                    return "Winter";
+                case 3:
+                    return "Spring";
+                case 4:
+                    return "Spring";
+                case 5:
+                    return "Spring";
+                case 6:
+                    return "Summer";
+                case 7:
+                    return "Summer";
+                case 8:
+                    return "Summer";
+                case 9:
+                    return "Autumn";
+                case 10:
+                    return "Autumn";
+                case 11:
+                    return "Autumn";
+                default:
+                    return "";
+            }
+        }
     }
 }
